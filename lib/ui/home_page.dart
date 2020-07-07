9862b319d2e14ef028a6473d889c56e4da839be0
Wesley Silva <wesley.sousa59@gmail.com>
2020-07-07 17:55:22 -0300

    homepage with listing of contacts are ok.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts/models/contact.dart';
 import 'package:contacts/repositories/contact_repository.dart';
 import 'package:flutter/material.dart';
@@ -35,12 +37,75 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
       ),
       body: ListView.builder(
-        padding: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(8.0),
         itemCount: _contacts.length,
         itemBuilder: (context, index) {
-          return;
+          return _contactCard(context, index);
         },
       ),
     );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: _getImageContact(_contacts[index].image),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      _contacts[index].name ?? '',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _contacts[index].email ?? '',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  Text(
+                    _contacts[index].phone ?? '',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ImageProvider _getImageContact(String image) {
+    if (image == null) {
+      return const AssetImage('images/person.png');
+    } else {
+      final _imageFile = File(image);
+      if (_imageFile.existsSync())
+        return FileImage(_imageFile);
+      else
+        return AssetImage('images/person.png');
+    }
   }
 }
